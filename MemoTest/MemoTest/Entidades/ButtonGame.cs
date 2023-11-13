e1daac54f263f6e25aa83e6e855db2ba38278c0e
LukySand <luky.sand@gmail.com>
2023-11-13 16:28:38 -0300

    changed the database and added button class

---
+++ MemoTest/MemoTest/Entidades/ButtonGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoTest.Entidades
+{
+    internal class ButtonGame : Button
+    {
+        public bool flipped { get; set; }
+
+    }
+}
